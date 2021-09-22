382442486ee337d3a3c2b716d568cac6a51ab2d1
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2021-09-22 12:06:56 +0000

    Update API documentation

--- app/src/main/java/com/project/network/ssugaeting/activity/ImageActivity.java
+++ app/src/main/java/com/project/network/ssugaeting/activity/ImageActivity.java
@@ -2,4 +2,11 @@ package com.project.network.ssugaeting.activity;
 
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
-import android.os.Bundle
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.project.network.ssugaeting.R;
+import com.project.network.ssugaeting.databinding.ActivityImageBinding;
+import com.squa
