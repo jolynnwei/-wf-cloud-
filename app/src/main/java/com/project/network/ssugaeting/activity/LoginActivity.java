ffae986809e4d5a121fb1861375c2463500ca9d3
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2021-09-24 09:09:10 +0000

    Automated master to rc merge

--- app/src/main/java/com/project/network/ssugaeting/activity/LoginActivity.java
+++ app/src/main/java/com/project/network/ssugaeting/activity/LoginActivity.java
@@ -2,3 +2,11 @@ package com.project.network.ssugaeting.activity;
 
 import android.app.ProgressDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import
