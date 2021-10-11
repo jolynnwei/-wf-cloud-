b2b482d500fbbabebb3b01698e9c6829a04a127d
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2021-10-11 22:53:19 +0000

    post

--- app/src/main/java/com/project/network/ssugaeting/activity/LoginActivity.java
+++ app/src/main/java/com/project/network/ssugaeting/activity/LoginActivity.java
@@ -131,3 +131,8 @@ public class LoginActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             progressDialog.dismiss();
+            if (result.equals("FAIL")) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                builder.setTitle("오류");
+                builder.setMessage("존재하지 않는 아이디이거나 비밀번호가 일치하지 않습니다.");
+        
