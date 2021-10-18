d4773fc56e662851a4de52241684441cb9f115fc
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2021-10-18 17:40:49 +0000

    farsync: fix info leak in ioctl

--- app/src/main/java/com/project/network/ssugaeting/activity/ModifyProfileActivity.java
+++ app/src/main/java/com/project/network/ssugaeting/activity/ModifyProfileActivity.java
@@ -14,3 +14,12 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Gallery;
+import android.widget.Toast;
+
+import com.project.netwo
