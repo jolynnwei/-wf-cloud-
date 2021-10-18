53ee51a6501b9138cd537b8e3980e92bdb862bc9
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2021-10-18 06:58:17 +0000

    update README files

--- app/src/main/java/com/project/network/ssugaeting/activity/ModifyProfileActivity.java
+++ app/src/main/java/com/project/network/ssugaeting/activity/ModifyProfileActivity.java
@@ -5,4 +5,12 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.databinding.Da
+import android.databinding.DataBindingUtil;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
