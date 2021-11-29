571c5c2055431fef7fc2404ec2814c433aca96ee
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2021-11-29 16:07:34 +0000

    fix README file

--- app/src/main/java/com/project/network/ssugaeting/activity/ModifyProfileActivity.java
+++ app/src/main/java/com/project/network/ssugaeting/activity/ModifyProfileActivity.java
@@ -267,3 +267,7 @@ public class ModifyProfileActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null && data.getData
