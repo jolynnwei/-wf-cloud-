9fa56eaead5542ba991a41fbd522515355a7291b
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2022-01-15 08:33:53 +0000

    commit message

--- app/src/main/java/com/project/network/ssugaeting/activity/SplashActivity.java
+++ app/src/main/java/com/project/network/ssugaeting/activity/SplashActivity.java
@@ -5,4 +5,13 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
-import android.view.animation
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.project.network.ssugaeting.R;
+import com.project.network.ssugaeting.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ActivitySplashBinding binding;
+    Animati
