a84f1c2c3c982edcbf53ccb818dcf6b179bfc24a
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2022-01-15 06:17:24 +0000

    md: Reassigned the parameters if read_seqretry returned true in func md_is_badblock.

--- app/src/main/java/com/project/network/ssugaeting/activity/SplashActivity.java
+++ app/src/main/java/com/project/network/ssugaeting/activity/SplashActivity.java
@@ -1,1 +1,8 @@
-packag
+package com.project.network.ssugaeting.activity;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.view.animation
