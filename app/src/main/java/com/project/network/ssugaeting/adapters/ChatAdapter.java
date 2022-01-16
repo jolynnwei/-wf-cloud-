199f5bbac04e47714e323924428563f33b352234
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2022-01-16 19:14:13 +0000

    Tweak again

--- app/src/main/java/com/project/network/ssugaeting/adapters/ChatAdapter.java
+++ app/src/main/java/com/project/network/ssugaeting/adapters/ChatAdapter.java
@@ -1,1 +1,10 @@
-packag
+package com.project.network.ssugaeting.adapters;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import
