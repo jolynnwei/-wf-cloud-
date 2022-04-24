d07169921dea66bcbfad5d4eb99e25482fd11728
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2022-04-24 19:11:42 +0000

    block:row: fix idling mechanism in ROW

--- app/src/main/java/com/project/network/ssugaeting/fragment/MyProfileFragment.java
+++ app/src/main/java/com/project/network/ssugaeting/fragment/MyProfileFragment.java
@@ -3,3 +3,11 @@ package com.project.network.ssugaeting.fragment;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.project.network.ssugaeting.R;
+import com.project.network.ssug
