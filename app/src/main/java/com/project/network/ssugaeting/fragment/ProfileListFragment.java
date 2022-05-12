c92471f0424cf7f8cd62cff4a3c944e433ffca85
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2022-05-12 08:40:45 +0000

    icon fixes

--- app/src/main/java/com/project/network/ssugaeting/fragment/ProfileListFragment.java
+++ app/src/main/java/com/project/network/ssugaeting/fragment/ProfileListFragment.java
@@ -3,4 +3,12 @@ package com.project.network.ssugaeting.fragment;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-im
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.project.network.ssugaeting.R;
+import com
