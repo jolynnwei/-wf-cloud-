752dc1492af916f41422864bf2409073b0cb6681
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2022-06-06 06:11:03 +0000

    Several changes.

--- app/src/main/java/com/project/network/ssugaeting/item/Chat.java
+++ app/src/main/java/com/project/network/ssugaeting/item/Chat.java
@@ -3,4 +3,16 @@ package com.project.network.ssugaeting.item;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-im
+import java.io.Serializable;
+
+/**
+ * Created by Jin on 2018-04-08.
+ */
+
+public class Chat implements Parcelable, Serializable {
+    private String sendMsg;
+    private String sendImageURI;
+    private String msgTime;
+    private int msgTurn;
+
+    public Chat(
