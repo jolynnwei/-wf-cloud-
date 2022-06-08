2486c8e11d1f9d815974f31fce9ff49c6443eef4
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2022-06-08 15:27:58 +0000

    Added link to docs

--- app/src/main/java/com/project/network/ssugaeting/item/ChatRoom.java
+++ app/src/main/java/com/project/network/ssugaeting/item/ChatRoom.java
@@ -3,4 +3,16 @@ package com.project.network.ssugaeting.item;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import java.util.ArrayList
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ * Created by Jin on 2018-05-30.
+ */
+
+public class ChatRoom implements Parcelable {
+    private ArrayList<Chat> chatList;
+    private Profile profile;
+    private int unCheckMsgCnt;
+
+    public ChatRoom(A
