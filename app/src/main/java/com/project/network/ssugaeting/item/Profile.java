0aea505e7e63a239b364c246e41b9c83b6bc1df8
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2022-06-18 05:59:13 +0000

    update dev-dependencies

--- app/src/main/java/com/project/network/ssugaeting/item/Profile.java
+++ app/src/main/java/com/project/network/ssugaeting/item/Profile.java
@@ -1,1 +1,14 @@
-package com.project.network.ssug
+package com.project.network.ssugaeting.item;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Jin on 2018-04-07.
+ */
+
+public class Profile implements Parcelable {
+    // Essential Information
+    private String id;
+    private String password;
+    private 
