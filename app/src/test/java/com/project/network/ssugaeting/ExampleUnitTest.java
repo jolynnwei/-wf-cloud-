e47c84e0c309e4206ba952d309abe25a7687ddc0
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2023-02-19 21:29:05 +0000

    Better error reporting.

--- app/src/test/java/com/project/network/ssugaeting/ExampleUnitTest.java
+++ app/src/test/java/com/project/network/ssugaeting/ExampleUnitTest.java
@@ -4,3 +4,15 @@ package com.project.network.ssugaeting;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
