c2b73dbfc84817d624714a5cf84a53013e6681ed
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2023-02-21 06:47:51 +0000

    gb

--- gradlew.bat
+++ gradlew.bat
@@ -14,3 +14,14 @@ set DEFAULT_JVM_OPTS=
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+@rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto init
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PA
