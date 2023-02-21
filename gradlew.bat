3a7c40e2ddab1c4235131b173a17bb2ace250576
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2023-02-21 06:45:53 +0000

    house keeping

--- gradlew.bat
+++ gradlew.bat
@@ -5,4 +5,12 @@
 @rem
 @rem ##########################################################################
 
-@rem Set local scope for the variables with windows NT shel
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS=
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
