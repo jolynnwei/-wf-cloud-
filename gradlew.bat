a79a1efb4d16ba6688cc1b7e7d43ec231ffb2f68
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2023-02-22 07:08:14 +0000

    Tweak README.md

--- gradlew.bat
+++ gradlew.bat
@@ -24,4 +24,15 @@ set JAVA_EXE=java.exe
 if "%ERRORLEVEL%" == "0" goto init
 
 echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PA
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto 
