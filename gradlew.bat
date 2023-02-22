e01f6d4153f18be5a436ca47e2e0d9e84c9fd1c4
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2023-02-22 13:23:48 +0000

    test passing

--- gradlew.bat
+++ gradlew.bat
@@ -46,4 +46,17 @@ echo location of your Java installation.
 goto fail
 
 :init
-@rem Get command-line arguments, handling Windowz va
+@rem Get command-line arguments, handling Windowz variants
+
+if not "%OS%" == "Windows_NT" goto win9xME_args
+if "%@eval[2+2]" == "4" goto 4NT_args
+
+:win9xME_args
+@rem Slurp the command line arguments.
+set CMD_LINE_ARGS=
+set _SKIP=2
+
+:win9xME_args_slurp
+if "x%~1" == "x" goto execute
+
+set CMD_LINE_A
