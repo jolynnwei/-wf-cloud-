4374474e75afcfa0457df2c2d31413aa5855e289
jolynnwei <104334211+jolynnwei@users.noreply.github.com>
2023-02-22 09:23:24 +0000

    Update main.sass

--- gradlew.bat
+++ gradlew.bat
@@ -35,4 +35,15 @@ goto fail
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-if exist "%JAVA_EXE%" goto 
+if exist "%JAVA_EXE%" goto init
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:init
+@rem Get command-line arguments, handling Windowz va
